bc5c34d7c8160fdd9e719ce84ef6a6f61a086438
jackhmorgan <117677338+jackhmorgan@users.noreply.github.com>
2024-07-09 17:19:10 -0500

    Add license

--- QHMM_AE/qhmm_ae.py
+++ QHMM_AE/qhmm_ae.py
@@ -1,3 +1,18 @@
+'''
+Copyright 2024 Jack Morgan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.circuit.library import BlueprintCircuit, EfficientSU2
 from qiskit.circuit import ParameterVector, Gate
